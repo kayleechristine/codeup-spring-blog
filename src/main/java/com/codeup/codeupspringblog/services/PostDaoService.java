6bfa2895a3007dcf053c88174b8d4befbce77fe9
Kaylee Morris <kaylee.christine.morris@gmail.com>
2023-02-24 23:21:32 -0600

    finished the form model binding exercise

--- src/main/java/com/codeup/codeupspringblog/services/PostDaoService.java
+++ src/main/java/com/codeup/codeupspringblog/services/PostDaoService.java
@@ -1,9 +1,13 @@
 package com.codeup.codeupspringblog.services;
 
 import com.codeup.codeupspringblog.models.Post;
+import com.codeup.codeupspringblog.models.User;
 import com.codeup.codeupspringblog.repositories.PostRepository;
 import com.codeup.codeupspringblog.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostDaoService {
@@ -16,17 +20,28 @@ public class PostDaoService {
         this.userRepository = userRepository;
     }
 
-    // CREATE
+    // CREATE / UPDATE
     public void savePost(Post post) {
-        post.setUser(userRepository.findById(1L).get().getUser());
+        post.setUser((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         postRepository.save(post);
     }
 
-
     // READ
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
 
-    // UPDATE
+    public Post getPostById(long id) {
+        if (postRepository.findById(id).isPresent()) {
+            return postRepository.findById(id).get();
+        } else {
+            throw new RuntimeException("Post was not found.");
+        }
+    }
 
     // DELETE
+    public void deletePostById(long id) {
+        postRepository.deleteById(id);
+    }
 
 }
